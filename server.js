9a95872c5320aa915ce4e83b0654adb45478aecc
Branden Ge <86636108+brandenge@users.noreply.github.com>
2022-08-25 14:42:01 -0500

    Weather and Movies APIs are working and talking to front end

--- server.js
+++ server.js
@@ -19,11 +19,13 @@ app.get('/', start);
 
 const getWeather = async (request, response) => {
   try {
+    console.log('TESTING WEATHER ENDPOINT');
     const lat = request.query.lat;
     const lon = request.query.lon;
     const weatherURL = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`;
     const dataToGroom = await axios.get(weatherURL);
     const dataToSend = dataToGroom.data.data.map(day => new Forecast(day));
+    console.log(dataToSend);
     response.status(200).send(dataToSend);
   } catch(error) {
     response.status(500).send(error.message);
@@ -44,6 +46,7 @@ const getMovies = async (request, response) => {
     const cityName = request.query.cityName;
     const moviesURL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${cityName}&page=1&include_adult=false`;
     const dataToGroom = await axios.get(moviesURL);
+    console.log('TESTING getMovies in BACK END');
     console.log(dataToGroom.data);
     const dataToSend = dataToGroom.data.results.map(movie => new Movie(movie));
     response.status(200).send(dataToSend);
@@ -56,11 +59,12 @@ app.get('/movies', getMovies);
 
 class Movie {
   constructor(movie) {
+    this.id = movie.id;
     this.title = movie.title;
     this.overview = movie.overview;
     this.average_votes = movie.vote_average;
     this.total_votes = movie.vote_count;
-    this.image_url = movie.poster_path;
+    this.image_url = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
     this.popularity = movie.popularity;
     this.released_on = movie.release_date;
   }
